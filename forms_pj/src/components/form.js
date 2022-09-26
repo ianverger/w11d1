eceab4c4af8256ee8c7e604e0cc7c1e8bdb4d2b1
Katherine <kat@Katherines-MacBook-Pro.local>
2022-09-26 14:56:13 -0400

    finish basic form

--- forms_pj/src/components/form.js
+++ forms_pj/src/components/form.js
@@ -7,35 +7,43 @@ const Form = () => {
     const [email, setEmail] = useState('');
     const [phone, setPhone] = useState('');
     const [phoneType, setPhoneType] = useState('');
+    const [staff, setStaff] = useState('');
+    const [bio, setBio] = useState('');
+    const [signup, setSignup] = useState(false);
 
 
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(email);
+        console.log(staff);
+        console.log(bio);
+        console.log(signup);
     };
 
     return (
     <form onSubmit={handleSubmit}>
         <label htmlFor="name">name</label>
-        <input id="name" 
-        type="text" 
-        placeholder="name" 
-        value={name} 
-        onChange={e => setName(e.target.value)}
+        <input 
+            id="name" 
+            type="text" 
+            placeholder="name" 
+            value={name} 
+            onChange={e => setName(e.target.value)}
         />
         <label htmlFor="email">email</label>
         <input id="email" 
-        type="text" 
-        placeholder="email" 
-        value={email} 
-        onChange={e => setEmail(e.target.value)}
+            type="text" 
+            placeholder="email" 
+            value={email} 
+            onChange={e => setEmail(e.target.value)}
         />
         <label htmlFor="phone">phone</label>
-        <input id="phone" 
-        type="text" 
-        placeholder="phone" 
-        value={phone} 
-        onChange={e => setPhone(e.target.value)}
+        <input 
+            id="phone" 
+            type="text" 
+            placeholder="phone" 
+            value={phone} 
+            onChange={e => setPhone(e.target.value)}
         />
         <label htmlFor="phoneType">phoneType</label>
         <select id="phoneType" value={phoneType} onChange={e => setPhoneType(e.target.value)}>
@@ -43,13 +51,38 @@ const Form = () => {
             <option value="work">Work</option>
             <option value="mobile">Mobile</option>
         </select>
-
-        {/* <DropdownButton id="phoneType" title="Phone Type">
-            <Dropdown.Item id="phoneType" value="home">Home</Dropdown.Item>
-            <Dropdown.Item id="phoneType" value="work">Work</Dropdown.Item>
-            <Dropdown.Item id="phoneType" value="mobile">Mobile</Dropdown.Item>
-        </DropdownButton> */}
-
+        <label htmlFor="staff">Instructor</label>
+        <input 
+            type="radio" 
+            id="Instructor"
+            name="staff"
+            value="Instructor" 
+            onChange={e => setStaff(e.target.value)}
+            />
+        <label htmlFor="staff">Student</label>
+        <input 
+            type="radio" 
+            id="Student"
+            name="staff"
+            value="Student" 
+            onChange={e => setStaff(e.target.value)}
+        />
+        <br></br>
+        <label htmlFor="bio">Bio</label>
+        <input
+            type="textarea"
+            id="bio"
+            value={bio}
+            onChange={e => setBio(e.target.value)}
+        />
+        <input
+            type="checkbox"
+            id="signup"
+            name="signup"
+            value={signup}
+            onChange={e => setSignup(!signup)}
+        />
+        <label htmlFor="signup">Sign up for email notifications!</label>
 
     <input type={'submit'} value="submit"/>
     </form>
